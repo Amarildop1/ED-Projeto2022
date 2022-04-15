093d6a235066efe4ca6469eef7f330d87d09389a
Amarildo <amarildop1dev@gmail.com>
2022-04-14 23:17:33 -0300

    gets de Carta

--- Carta.py
+++ Carta.py
@@ -5,6 +5,12 @@ class Carta:
         self.__naipe = naipe
         self.__cor = cor
 
+    def get_numero(self):
+        return self.__numero
+    
+    def get_naipe(self):
+        return self.__naipe
+
 
     def __str__(self):
-        return f'{self.__numero} de {self.__naipe}'
+        return f'{self.get_numero()} de {self.get_naipe()}'
