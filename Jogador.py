1181ef98df6bdff255587394cd6143c8faa15a59
Amarildo <amarildop1dev@gmail.com>
2022-04-15 23:19:15 -0300

    Classe Jogador

--- Jogador.py
+++ Jogador.py
@@ -2,3 +2,19 @@ class Jogador:
 
     def __init__(self, nome):
         self.__nome = nome
+
+
+    def getNome(self):
+        return self.__nome
+
+
+    def puxarCarta(self):
+        pass
+    
+
+    def receberCartas():
+        pass
+
+
+    def __str__(self):
+        return f'Jogador: {self.getNome()}'
