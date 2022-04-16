a9e8eb2b4d725110d7db10abb4e1bef487feb6dc
Amarildo <amarildop1dev@gmail.com>
2022-04-15 23:10:00 -0300

    Testando o método embaralhar criando instâncias no arquivo principal

--- Batalha.py
+++ Batalha.py
@@ -1,7 +1,27 @@
 # Classe principal de início do Jogo
 from Baralho import Baralho
+from PilhaEncadeada import Pilha
 
 if __name__ == "__main__":
-    batalha1 = Baralho()
-    print(batalha1)
+
+    baralho1 = Baralho()
+    #print("EM ORDEM: \n")
+    #print(baralho1)
+
+    #print("===========================")
+
+    baralho1.embaralhar()
+    print("EMBARALHADO: \n")
+    print(baralho1)
+
+    print("==========================")
+
+
+    p = Pilha()
+
+    print("Pilha Geral com o baralho: \n")
+
+    p.empilha(baralho1)
+
+    print(p.imprime())
 
