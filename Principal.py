d70d740fdd9713eaa073e47b86fd9f7d51d5ad3c
Amarildo <amarildop1dev@gmail.com>
2022-04-18 22:35:23 -0300

    Adicionando documentação do código

--- Principal.py
+++ Principal.py
@@ -1,4 +1,10 @@
-# Arquivo principal de início do Jogo
+""" Jogo: Batalha de Cartas
+    Versão: 1.0
+    Autor: Amarildo
+    Ano: 2022
+    > Fins didáticos da disciplina de Estrutura de Dados do IFPB
+"""
+
 from PilhaEncadeada import Pilha
 from Jogador import Jogador
 from Batalha import Batalha
@@ -10,6 +16,11 @@ play2 = Jogador("Player 2")
 
 
 def informacaoSobreOjogo():
+    """ Função que exibe na tela informações e instruções.
+
+        Não espera parâmetros e não tem retorno.
+    """
+
     print("\n\n - - - - INFORMAÇÕES SOBRE O JOGO - - - - ")
     print("""
 > Para iniciar o jogo: Digite 1
@@ -21,6 +32,13 @@ def informacaoSobreOjogo():
 
 
 def revelarCampeao():
+    """ Função que determina quem venceu o jogo.
+
+        Não requer parâmetros.
+
+        Retorna uma String formatada com o nome do jogador que venceu 
+        ou informa se houve empate.
+    """
     if ( play1.getTotalDeCartas() > play2.getTotalDeCartas() ):
         return f'{play1.getNome()} VENCEU A BATALHA!'
     if ( play1.getTotalDeCartas() < play2.getTotalDeCartas() ):
@@ -31,6 +49,18 @@ def revelarCampeao():
 
 # FUNÇÃO INICIAR JOGO
 def iniciarJogo():
+    """ Função principal para iniciar o jogo.
+
+        Não requer parâmetros de entrada.
+
+        Não tem retorno.
+
+        Cria a "mesa" do jogo. 
+        
+        É responsável por inicializar e começar a batalha.
+
+        Possui uma função especial aninhada: __cmp__(self, other)
+    """
     print("\n\n\n * * * * * * * * * * * * INICIO DO JOGO * * * * * * * * * * * * \n")
 
     # Criando a Batalha/Mesa
@@ -88,20 +118,30 @@ def iniciarJogo():
         #AINDA NÃO ESTÁ CERTO
         #AS VITÓRIAS NÃO ESTÃO SEMPRE OK
         def __cmp__(self, other):
+            """ Função que faz a comparação das cartas.
+
+                Recebe dois parâmetros: self e other.
+
+                Retorna uma String formatada.
+
+                Compara atributos entre os 2 parâmetros e diz um retorno escolhido.
+
+                Determina quem venceu cada rodada com base em uma regra escolhida.
+            """
             if( self.getNumero() > other.getNumero() ):
                 play1.conquistouUmaCarta(mesa.desempilha())
                 play1.conquistouUmaCarta(mesa.desempilha())
                 if not batalha1.cartasBloqueadasPeloEmpate.estaVazia():
                     batalha1.distribuirCartasBloqueadas(play1)
                     batalha1.distribuirCartasBloqueadas(play1)
-                return f'\n - - - - - - - ->  \o/  {play1.getNome()}  GANHOU \o/  <- - - - - - - -\n\n'
+                return f'\n\n - - - - - - - ->  \o/  {play1.getNome()}  GANHOU \o/  <- - - - - - - -\n\n'
             if( self.getNumero() < other.getNumero() ):
                 play2.conquistouUmaCarta(mesa.desempilha())
                 play2.conquistouUmaCarta(mesa.desempilha())
                 if not batalha1.cartasBloqueadasPeloEmpate.estaVazia():
                     batalha1.distribuirCartasBloqueadas(play2)
                     batalha1.distribuirCartasBloqueadas(play2)
-                return f'\n - - - - - - - ->  \o/  {play2.getNome()}  GANHOU \o/  <- - - - - - - -\n\n'
+                return f'\n\n - - - - - - - ->  \o/  {play2.getNome()}  GANHOU \o/  <- - - - - - - -\n\n'
             else:
                 batalha1.cartasBloqueadasPeloEmpate.empilha(mesa.desempilha())
                 batalha1.cartasBloqueadasPeloEmpate.empilha(mesa.desempilha())
@@ -112,6 +152,7 @@ def iniciarJogo():
 
         print(__cmp__(play1Retirou, play2Retirou))
 
+        # Só vai exibir essa pilha se não estiver vazia.
         if not batalha1.cartasBloqueadasPeloEmpate.estaVazia():
             print(f'Cartas Bloqueadas por empate: {batalha1.imprimirCartasBloqueadas()}\n')
 
@@ -124,7 +165,7 @@ def iniciarJogo():
         print(f'- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\n')
 
 
-    # DEFININDO O CAMPEÃO
+    # EXIBINDO O CAMPEÃO
     print(f'\t \o/  {revelarCampeao()}  \o/ \n')
 
     print(f'\nTotal final: {play1.getTotalDeCartas()} cartas com {play1.getNome()}')
@@ -134,14 +175,21 @@ def iniciarJogo():
 ######################################################################################
 
 
-# SE PRESSIONAR ENTER SEM NÚMERO DE OPERAÇÃO DÁ EXCEÇÃO: 
-#ValueError: invalid literal for int() with base 10: 'g'
-# SE PRESSIONAR ALGUMA LETRA DÁ EXCEÇÃO: 
-#ValueError: invalid literal for int() with base 10: 'h'
-#SE APÓS UM VENCEDOR, O USER ESCOLHER 1 PARA INICIAR OUTRA PARTIDA, A PILHA ESTARÁ VAZIA
+#SE APÓS UM VENCEDOR, SE O USER ESCOLHER 1 PARA INICIAR OUTRA PARTIDA, A PILHA ESTARÁ VAZIA
 #DÁ A EXCEÇÃO DE PILHA VAZIA
+def mostrarMenu():
+    """Função que exibe o menu com as opções disponíveis e faz as chamadas correspondentes.
+    
+    Essa função não tem retorno e não espera parâmetros.
 
-def mostrarMenu(): 
+    Variáveis:
+        escolha: É a variável de controle para usar no menu com a operação escolhida.
+
+    1: Inicia o jogo
+    2: Altera o nome padrão dos jogadores
+    3: Mostra informações sobre o jogo
+    0: Encerra a aplicação
+    """
     escolha = True
 
     while escolha != 0 :
@@ -169,8 +217,8 @@ def mostrarMenu():
             print("\n\nINFORME UM NÚMERO INTEIRO E VÁLIDO DISPONÍVEL NO MENU <-----")
 
 
-########################## P R I N C I P A L ##########################
 
+########################## P R I N C I P A L ##########################
 if __name__ == "__main__":
 
     print("\n # # # # # # # # # # JOGO BATALHA DE CARTAS # # # # # # # # # #")
